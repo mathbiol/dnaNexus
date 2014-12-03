f0a0d60f55751ec89475c3411031c3aa54a40829
Jonas Almeida <jalmeida@mathbiol.org>
2014-12-03 13:20:09 -0600

    APIkey in local storage rather than shared

--- jonas.js
+++ jonas.js
@@ -1,1 +1,9 @@
 console.log('jonas.js')
+if(!localStorage.getItem('dnaNexusAPIkey')){
+    localStorage.setItem('dnaNexusAPIkey',prompt('Your DNAnexus API key:'))
+}
+
+var api = new DX.Api(localStorage.getItem('dnaNexusAPIkey'));
+    api.call("user-jonasalmeida", "describe").done(function(userData) {
+        console.log("user-jonasalmeida's full name is " + [userData.first, userData.last].join(" "));
+});
